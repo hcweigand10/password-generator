aa34322fa24608decd9bcd79d9513791bc289428
Henry Weigand <henryweigand10@gmail.com>
2021-12-23 09:58:10 -0800

    add comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,16 +18,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// builds password one character at a time from available character string
 function generatePassword () {
   var password = "";
   for ( var i = 0; i < length; i++ ) {
     password += characters.charAt(Math.floor(Math.random() * 
     characters.length));
   }
-  console.log(password);
   return password;
 }
 
+// asks user a series of questions to determine parameters of password. Exits the loop if user wants an absurdly long password, and restarts the loop if the user wants a short password. The loop is also restarted if no paraters are selected, accompanied by an error message
 function getParameters() {
   characters = "";
   length = prompt("How long would you like it to be?\n(Between 8 and 128 characters)");
@@ -36,7 +37,7 @@ function getParameters() {
     getParameters();
   } else if (length > 128) {
     alert("If you're looking for more security than 128 characters I suggest you look elsewhere than a two-week-old CS stduent.");
-    return false;
+    return null;
   }
   if (confirm("Do you want lower case letters?")) {
     characters += "abcdefghijklmnopqrstuvwxyz";
